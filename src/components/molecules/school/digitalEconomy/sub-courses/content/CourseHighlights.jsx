832a0d0501f00d8db63ceebc2ac92f0ecd65a316
Vchuks <64593598+Vchuks@users.noreply.github.com>
2023-11-10 10:06:47 +0100

    changes

--- src/components/molecules/school/digitalEconomy/sub-courses/content/CourseHighlights.jsx
+++ src/components/molecules/school/digitalEconomy/sub-courses/content/CourseHighlights.jsx
@@ -52,7 +52,7 @@ const CourseHighlights = () => {
             N {numFor.format(digital[2]?.ngn_price)}
             <span className="text-cgray text-xl lg:text-2xl font-[gmedium]">
               {" "}
-              | N <span className="line-through">{numFor.format(digital[2]?.ngn_offset)}</span>
+              | N <span className="line-through">{numFor.format(digital[2]?.ngn_offset + digital[2]?.ngn_price)}</span>
             </span>
           </p>
           <Text
@@ -67,7 +67,7 @@ const CourseHighlights = () => {
           </div>
         <div className="flex flex-col gap-6 my-4">
             <hr className="w-full mt-2 mb-3"/>
-            <TextLink to="https://lively-cocada-4d1086.netlify.app/106" target="_blank" body={<button className="p-4 w-full bg-corange font-[gsemibold] rounded-[10px] text-white">
+            <TextLink to={`https://lively-cocada-4d1086.netlify.app?course_name=${digital[2]?.name}&cohort=${cohort[0]?.name}`} body={<button className="p-4 w-full bg-corange font-[gsemibold] rounded-[10px] text-white">
               Pay N {numFor.format(digital[0]?.ngn_price)}
             </button>} />
             <TextLink target='_blank' to="https://pluralcode.academy/chat-advisor" className='w-full' body={<button className="w-full border p-4 rounded-[10px] bg-white text-corange border-corange">Chat with Student Advisor</button>} />
